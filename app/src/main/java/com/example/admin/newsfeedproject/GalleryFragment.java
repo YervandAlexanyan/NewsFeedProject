3ab85e295daab5ef2bf177757e2786abdf7690ab
Yervand <yalexanyan@mail.ru>
2017-03-25 18:51:50 -0700

    Commit

---
+++ app/src/main/java/com/example/admin/newsfeedproject/GalleryFragment.java
@@ -0,0 +1,20 @@
+package com.example.admin.newsfeedproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Admin on 3/25/2017.
+ */
+
+public class GalleryFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gallery_fragment,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/admin/newsfeedproject/SearchFragment.java
@@ -0,0 +1,20 @@
+package com.example.admin.newsfeedproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Admin on 3/25/2017.
+ */
+
+public class SearchFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.search_fragment,container,false);
+    }
+}
